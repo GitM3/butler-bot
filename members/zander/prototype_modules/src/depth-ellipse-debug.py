485fd70dd713f4098bfb29a50f036c9bf7714384
GitM3 <zander@polsons.info>
2025-11-26 16:41:58 +0900

    Depth mask alignment, rs integration, stretching kerne

--- members/zander/prototype_modules/src/depth-ellipse-debug.py
+++ members/zander/prototype_modules/src/depth-ellipse-debug.py
@@ -12,15 +12,53 @@ align = rs.align(rs.stream.color)
 
 pipeline.start(config)
 
-window_mask = "Depth Mask"
-window_overlay = "Overlay"
+window = "Overlay"
 
-cv2.namedWindow(window_mask, cv2.WINDOW_NORMAL)
-cv2.namedWindow(window_overlay, cv2.WINDOW_NORMAL)
+cv2.namedWindow(window, cv2.WINDOW_NORMAL)
 
-cv2.createTrackbar("Depth Thresh", window_mask, 1000, 5000, lambda x: None)
-cv2.createTrackbar("Alpha", window_mask, 50, 100, lambda x: None)
+cv2.createTrackbar("Depth Thresh", window, 1000, 5000, lambda x: None)
+cv2.createTrackbar("Alpha", window, 50, 100, lambda x: None)
+cv2.createTrackbar("kernel", window, 1, 50, lambda x: None)
+cv2.createTrackbar("c1", window, 1, 150, lambda x: None)
+cv2.createTrackbar("c2", window, 1, 150, lambda x: None)
 
+def find_ellipse_from_masked_rgb(mask, rgb):
+    masked = cv2.bitwise_and(rgb, rgb, mask=mask)
+    gray = cv2.cvtColor(masked, cv2.COLOR_BGR2GRAY)
+    gray_blur = cv2.GaussianBlur(gray, (5, 5), 0)
+    c1  = cv2.getTrackbarPos("c1", window)
+    c2  = cv2.getTrackbarPos("c2", window)
+    edges = cv2.Canny(gray_blur, c1, c2)
+
+    edges = cv2.bitwise_and(edges, edges, mask=mask)
+
+    contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    best_ellipse = None
+    best_area = 0
+
+    for c in contours:
+        if len(c) < 5:
+            continue
+
+        area = cv2.contourArea(c)
+        if area < 200:     # reject small/noisy
+            continue
+
+        ellipse = cv2.fitEllipse(c)
+
+        if area > best_area:
+            best_area = area
+            best_ellipse = ellipse
+
+    return best_ellipse
+
+def draw_ellipse(img, ellipse, color=(0,255,0), thickness=2):
+    if ellipse is None:
+        return img
+    output = img.copy()
+    cv2.ellipse(output, ellipse, color, thickness)
+    return output
 
 try:
     while True:
@@ -36,17 +74,23 @@ try:
         depth_image = np.asanyarray(depth_frame.get_data())      # uint16 depth in mm
         color_image = np.asanyarray(color_frame.get_data())      # RGB aligned to depth
 
-        depth_thresh_mm = cv2.getTrackbarPos("Depth Thresh", window_mask)
-        alpha_percent   = cv2.getTrackbarPos("Alpha", window_mask)
+        depth_thresh_mm = cv2.getTrackbarPos("Depth Thresh", window)
+        alpha_percent   = cv2.getTrackbarPos("Alpha", window)
+        kernel_size  = cv2.getTrackbarPos("kernel", window)
         alpha = alpha_percent / 100.0
 
-        mask = ((depth_image > 0) & (depth_image <= depth_thresh_mm)).astype(np.uint8) * 255
-        cv2.imshow(window_mask, mask)
+        init_mask = ((depth_image > 0) & (depth_image <= depth_thresh_mm)).astype(np.uint8) * 255
+        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (kernel_size,kernel_size))
+        mask = cv2.dilate(init_mask, kernel, iterations=1)
 
         mask_3ch = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
+        ellipse = find_ellipse_from_masked_rgb(mask, color_image)
 
+        vis = draw_ellipse(color_image, ellipse)
+        cv2.imshow("Overlay", vis)
         overlay = cv2.addWeighted(color_image, 1 - alpha, mask_3ch, alpha, 0)
-        cv2.imshow(window_overlay, overlay)
+        images = np.hstack((vis, overlay))
+        cv2.imshow(window, images)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
