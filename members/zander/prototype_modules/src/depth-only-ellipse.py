e69747b80e974f3c64e68d3f405b983514e07979
GitM3 <zander@polsons.info>
2025-12-07 20:55:08 +0900

    depth based on roi

--- members/zander/prototype_modules/src/depth-only-ellipse.py
+++ members/zander/prototype_modules/src/depth-only-ellipse.py
@@ -5,7 +5,7 @@ import pyrealsense2 as rs
 pipeline = rs.pipeline()
 config = rs.config()
 
-config.enable_device_from_file("/home/zander/Documents/20251202_164751.bag")
+config.enable_device_from_file("/home/zander/Documents/20251202_164616.bag")
 
 align = rs.align(rs.stream.color)
 
@@ -66,13 +66,52 @@ try:
         kernel_size = cv2.getTrackbarPos("Kernel", window)
         kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (kernel_size,kernel_size))
 
-
+        # Sol 0
+        # Manual depth
         init_mask = ((depth > 0) & (depth <= depth_thresh)).astype(np.uint8) * 255
+
+        # SOl 1:
+        # Dynamic depth slice: find local minimum depth in center ROI
+        h, w = depth.shape
+        roi = depth[h//4:3*h//4, w//4:3*w//4]
+        d_min = np.percentile(roi[roi > 0], 2)  # robust minimum depth
+        d_min = min(d_min,depth_thresh)
+        margin = cv2.getTrackbarPos("Margin (mm)", window)
+
+        dynamic_mask = ((depth >= d_min) & (depth <= d_min + margin)).astype(np.uint8) * 255
+        dynamic_mask = cv2.morphologyEx(dynamic_mask, cv2.MORPH_CLOSE, kernel)
+        init_mask = dynamic_mask
+
         mask = cv2.dilate(init_mask, kernel, iterations=1)
         close_mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
 
-        contours, _ = cv2.findContours(close_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        # Sol 2:
+        # K-Mean
+        # base_mask = ((depth > 0) & (depth <= depth_thresh))
+        # vals = depth[base_mask]
+        #
+        # if vals.size == 0:
+        #     close_mask = np.zeros_like(depth, dtype=np.uint8)
+        # else:
+        #     samples = vals.reshape(-1, 1).astype(np.float32)
+        #
+        #     K = 4  # number of depth layers
+        #     criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+        #     ret, labels, centers = cv2.kmeans(samples, K, None, criteria, 3, cv2.KMEANS_PP_CENTERS)
+        #
+        #     centers = centers.flatten()
+        #     nearest_idx = np.argmin(centers)  # smallest depth = closest
+        #
+        #     # Rebuild a label image with same size as depth
+        #     labels_full = np.full(depth.shape, -1, dtype=np.int32)
+        #     labels_full[base_mask] = labels.flatten()
+        #
+        #     close_mask = (labels_full == nearest_idx).astype(np.uint8) * 255
+        #     close_mask = cv2.morphologyEx(close_mask, cv2.MORPH_CLOSE, kernel)
+        #
 
+        contours, _ = cv2.findContours(close_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        debug_color = np.zeros_like(color)
         if len(contours) > 0:
             c = max(contours, key=cv2.contourArea)
             M = cv2.moments(c)
@@ -80,12 +119,13 @@ try:
                 cx = int(M["m10"]/M["m00"])
                 cy = int(M["m01"]/M["m00"])
                 cv2.putText(color, "X", (cx,cy), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255,0,0), 2)
-        
+                cv2.drawContours(debug_color, [c], -1, (0, 0, 255), 2)  # red contour
+
+        debug_blend = cv2.addWeighted(color, 1 - alpha2, debug_color, alpha2, 0)
         mask_color = cv2.cvtColor(close_mask, cv2.COLOR_GRAY2BGR)
         overlay = cv2.addWeighted(color, 1 - alpha1, mask_color, alpha1, 0)
-        #overlay = cv2.addWeighted(overlay, 1 - alpha2, debug_color, alpha2, 0)
-
-        combined = np.hstack((color, overlay))
+        final_display = cv2.addWeighted(overlay, 1 - alpha2, debug_color, alpha2, 0)
+        combined = np.hstack((color, final_display))
         cv2.imshow(window, combined)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
